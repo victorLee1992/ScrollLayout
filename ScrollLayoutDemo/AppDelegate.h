082f582bd1407e5254784ce41e2e52251ef5c994
lixiong <lixiong@lixiongdemacbook-pro.local>
2018-10-25 15:26:34 +0800

    Initial Commit

---
+++ ScrollLayoutDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ScrollLayoutDemo
+//
+//  Created by lixiong on 2018/10/25.
+//  Copyright © 2018年 xiongli. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ScrollLayoutDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ScrollLayoutDemo
+//
+//  Created by lixiong on 2018/10/25.
+//  Copyright © 2018年 xiongli. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
